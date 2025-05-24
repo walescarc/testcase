2e9ae02b87830d8f5d7be5a0a18146ebd41424d7
Walesca <walescarc@gmail.com>
2025-05-24 12:42:08 -0300

    add sony vaio i7

--- demoblaze.py
+++ demoblaze.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
@@ -7,3 +8,21 @@ driver = webdriver.Chrome()
 driver.get("https://www.demoblaze.com/index.html") 
 
 time.sleep(2)
+
+# Clicar no link com texto "Sony vaio i7"
+elemento = driver.find_element(By.LINK_TEXT, "Sony vaio i7")
+elemento.click()
+
+time.sleep(2)
+
+botao = driver.find_element(By.LINK_TEXT, "Add to cart")
+botao.click()
+
+time.sleep(2)
+
+alerta = driver.switch_to.alert
+print(alerta.text) 
+alerta.accept() 
+
+time.sleep(3)
+driver.quit()
