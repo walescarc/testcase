1e2cd3e7b238c69f0135ff5ac7e07ee820ddc475
Walesca <walescarc@gmail.com>
2025-05-24 12:47:42 -0300

    add iphone6

--- demoblaze.py
+++ demoblaze.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import time
+
 
 driver = webdriver.Chrome()
 
@@ -25,4 +28,25 @@ print(alerta.text)
 alerta.accept() 
 
 time.sleep(3)
-driver.quit()
+
+# Por seletor CSS, mais confiável:
+home_link = driver.find_element(By.CSS_SELECTOR, 'a.nav-link[href="index.html"]')
+home_link.click()
+
+time.sleep(2)
+
+elemento = driver.find_element(By.LINK_TEXT, "Iphone 6 32gb")
+elemento.click()
+
+time.sleep(2)
+
+botao = driver.find_element(By.LINK_TEXT, "Add to cart")
+botao.click()
+
+time.sleep(2)
+
+alerta = driver.switch_to.alert
+print(alerta.text)  
+alerta.accept()
+
+time.sleep(3)
